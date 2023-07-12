8ee7ed922097618a7d81378a89555d167dcd92ab
AnthonyCyr <acyr@egyde.ca>
2023-07-12 16:19:39 -0400

    s3 bucket policy update

--- ermetic_aws_terraform_onboarding/main.tf
+++ ermetic_aws_terraform_onboarding/main.tf
@@ -63,5 +63,37 @@ resource "aws_s3_bucket" "ermetic_trail_bucket" {
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.ermetic_trail_bucket.id
-  policy = file("${path.module}/s3_bucket_policy.json")
+  # policy = file("${path.module}/s3_bucket_policy.json")
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AWSCloudTrailAclCheck",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.ermetic_trail_bucket.id}"
+    },
+    {
+      "Sid": "AWSCloudTrailWrite",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.ermetic_trail_bucket.id}/AWSLogs/${local.account_id}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    }
+    ]
+  }
+EOF
 }
+
